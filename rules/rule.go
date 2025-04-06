35ae60cff054bcac6153555ef6af0cf4032d5b83
Kenechukwu <kene@kene>
2025-04-06 23:56:01 +0100

    uploaded file as an artifact

--- rules/rule.go
+++ rules/rule.go
@@ -18,4 +18,16 @@ type Rule struct {
 // ReportIssue prints a scan issue in a standard format
 func ReportIssue(file string, message string, severity string) {
 	fmt.Printf("[%s] %s: %s\n", severity, file, message)
+
+	// Output to log file
+	logFile, err := os.OpenFile("scan-results.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening log file: %v\n", err)
+		return
+	}
+	defer logFile.Close()
+
+	logMsg := fmt.Sprintf("[%s] %s: %s\n", severity, file, message)
+	logFile.WriteString(logMsg)
 }
+
