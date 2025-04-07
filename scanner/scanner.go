342f9fe5300be3ded095baca25190aba841f6e53
Kenechukwu <kene@kene>
2025-04-07 01:20:38 +0100

    edited Print statement in scanner.go file

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -24,7 +24,7 @@ func RunScanner(root string){
 		
 		case ".yaml", ".yml":
 			scanKubernetes(path)
-			fmt.Printf("This is a Kubernetes file")
+			fmt.Printf("This is a Kubernetes file\n")
 		
 		case "":
 			if filepath.Base(path) == "Dockerfile"{
